8284955e89a5d932d1fc892194f2afe83e7b10ea
larrysmith <larrypsmith415@gmail.com>
2020-09-13 20:13:05 -0700

    Open Gradsnapp workbook and create dict of students

--- code.py
+++ code.py
@@ -1,11 +1,38 @@
 import openpyxl
 import psycopg2
 
-def print_cells(path):
-  wb = openpyxl.load_workbook(filename=path, read_only=True)
-  ws = wb.active
-  for value in ws.values:
-    print(value)
+# Local development
+# CONNECTION_PARAMETERS = {
+#   'host': 'postgres',
+#   'dbname': 'postgres',
+#   'user': 'postgres',
+#   'password': 'postgrespassword',
+#   'port': '5432'
+# }
+
+# with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+#   with conn.cursor() as curs:
+#     print(curs.execute('SELECT * FROM INFORMATION_SCHEMA.TABLES'))
+
+def get_active_worksheet(path):
+  # Open workbook as read-only
+  wb = openpyxl.load_workbook(filename=path, read_only=True, data_only=True)
+
+  # Get first worksheet
+  return wb.active
+
+def get_data(ws):
+  # Get column names
+  fields = list(ws.values)[0]
+
+  # Generate dict from records
+  return [
+    {field_name: value for (field_name, value) in zip(fields, row)}
+    for row in ws.values
+  ]
+
+
 
 if __name__ == "__main__":
-  print_cells('fwddata/Gradsnapp Data - Cleaned of Personal Identifable Info (No Phone Email).xlsx')
+  ws = get_active_worksheet('fwddata/Gradsnapp Data - Cleaned of Personal Identifable Info (No Phone Email).xlsx')
+  print(get_data(ws)[1])
